1ec39446c7e0537bd9de576a15b5fa17e52d7b2f
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-03-11 20:22:22 -0500

    redirect to root if route doesn't exist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,5 @@ import { ProductsComponent } from './products/products.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'productsCRUD', component: ProductsComponent },
+  { path: '**', redirectTo: '' },
 ];
